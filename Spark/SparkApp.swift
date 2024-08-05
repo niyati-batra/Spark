2de1144c086c19acdc6a98b0fd4fd8cdf4971a95
niyati-batra <niyatibatra5@gmail.com>
2024-08-05 15:19:12 -0400

    Initial Commit

---
+++ Spark/SparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkApp.swift
+//  Spark
+//
+//  Created by Niyati Batra on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
